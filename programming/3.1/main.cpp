271eb9bc060db20129f5c8ef22897d1a1d18f04b
Petryk Oleksandr <sachapos50@gmail.com>
2022-02-17 12:10:18 +0200

    Savew changse

---
+++ programming/3.1/main.cpp
@@ -0,0 +1,29 @@
+// #include <iostream>
+
+
+// using namespace std;
+
+// // 1 rod - - 
+// float rod_square = 28.3;
+// float rod_funt = 1000;
+
+// void drawTable(int count) {
+//   for (int i = 1; i < count; i++) {
+//     cout << "|" << i << " rod |\t" <<  i * rod_square << " square |\t" << i * rod_funt << " funt |\t" << endl;
+//   }
+// }
+
+
+// int main() {
+//   int count;
+
+//   cout << "Enter count of rows: ";
+//   cin >> count;
+
+//   drawTable(count);
+
+//   return 0;
+// }
+
+
+
